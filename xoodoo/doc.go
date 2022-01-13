a561bf676e7da0037e5d39f92ebefee25e14b3af
Calvin McCoy <calvin@inmcm.dev>
2022-01-12 19:02:56 -0800

    add xoodoo documenation header

---
+++ xoodoo/doc.go
@@ -0,0 +1,7 @@
+// Package xoodoo implements the Xoodoo cryptographic permutation function from which a variety of
+// other cryptographic primitives and modes can be build. Xoodoo operates on a 384-bit state, realized
+// here as array of twelve(12) 32-bit unsigned integers, to generate a new pseudo-random state each time
+// the permutation is applied. In addition to the main constructor and permutation functions, a variety
+// of other helper methods are provided to manipulate the underlying state bytes.
+//
+package xoodoo
